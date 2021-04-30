22cda7892eef4e6060804bc88177dbc0f4cd3589
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-30 06:15:10 +0530

    interval selection implementation

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const textarea = document.getElementById("textarea");
 const tags = document.getElementById("tags");
+const decide = document.getElementById("button");
+const selectionDelay = 0;
+const currentElasped = 0;
 
 textarea.addEventListener("keyup", (e) => {
     const options = e.target.value
@@ -19,3 +22,28 @@ textarea.addEventListener("keyup", (e) => {
         tags.appendChild(tag);
     });
 });
+
+decide.addEventListener("click", () => {
+    selectOneRandom();
+});
+
+function selectOneRandom() {
+    const options = tags.childNodes;
+    selectionDelay = options.length;
+
+    const intervalFunction = setInterval(() => {
+        highlightOneRandomly(options);
+    }, 100);
+
+    if (currentElasped == selectionDelay) {
+        clearInterval(intervalFunction);
+    }
+}
+
+function highlightOneRandomly(options) {
+    currentElasped++;
+
+    options[Math.floor(Math.random() * options.length)].classList.add(
+        "highlight"
+    );
+}
